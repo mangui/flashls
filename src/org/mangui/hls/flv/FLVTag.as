mangui/flashls

commit 4147ac4b5a3c0036b557ba4fd3675a44c01bf2fe

FLVTag : build FLV tag in demuxer timer
progressively build FLV tag data in demuxer timer, instead of building all of them in a row, in HLSNetStream.appendTags()
this avoids occupancy spike (observed using Adobe Scout) and potential frame drop while executing HLSNetStream.appendTags().

src/org/mangui/hls/flv/FLVTag.as
@@ -34,6 +34,10 @@ package org.mangui.hls.flv {
         public var dts : Number;
         /** Type of FLV tag.**/
         public var type : int;
+        /* built data */
+        protected var builtData : ByteArray;
+        /* payload length */
+        protected var length : int;
 
         /** Get the FLV file header. **/
         public static function getHeader() : ByteArray {
@@ -55,12 +59,14 @@ package org.mangui.hls.flv {
             // PreviousTagSize0
             flv.writeUnsignedInt(0);
             return flv;
-        };
+        }
 
         /** Get an FLV Tag header (11 bytes). **/
-        public static function getTagHeader(type : int, length : int, stamp : int) : ByteArray {
-            var tag : ByteArray = new ByteArray();
-            tag.length = 11+length+4;
+        private static function updateTagHeader(tag: ByteArray, type : int, length : int, stamp : int) : void {
+            if(tag.length == 0) {
+                // set exact length only if not specified already
+                tag.length = 11+length+4;
+            }
             tag.writeByte(type);
 
             // Size of the tag in bytes after StreamID.
@@ -77,8 +83,8 @@ package org.mangui.hls.flv {
             tag.writeByte(0);
             tag.writeByte(0);
             // All done
-            return tag;
-        };
+            return;
+        }
 
         /** Save the frame data and parameters. **/
         public function FLVTag(typ : int, stp_p : Number, stp_d : Number, key : Boolean) {
@@ -86,21 +92,53 @@ package org.mangui.hls.flv {
             pts = stp_p;
             dts = stp_d;
             keyframe = key;
+            length = 0;
         }
-        ;
 
         /** Returns the tag data. **/
         public function get data() : ByteArray {
-            var array : ByteArray;
-            /* following specification http://download.macromedia.com/f4v/video_file_format_spec_v10_1.pdf */
+            if(builtData) {
+                // update header (PTS/DTS may have changed because of keyframe/accurate seeking)
+                buildHeader(builtData);
+            } else {
+                build();
+            }
+            return builtData;
+        }
 
+        /** Build tag data (including header) **/
+        public function build() : void {
+            if(!builtData) {
+                var array : ByteArray = new ByteArray();
+                buildHeader(array);
+                if(pointers) {
+                    for each(var pointer : TagData in pointers) {
+                        if (type == AVC_NALU) {
+                            array.writeUnsignedInt(pointer.length);
+                        }
+                        array.writeBytes(pointer.array, pointer.start, pointer.length);
+                    }
+                    // save memory, free pointers (PES payload)
+                    pointers = null;
+                }
+                // Write previousTagSize and return data.
+                array.writeUnsignedInt(array.length);
+                builtData = array;
+            }
+        }
+
+        /** build/update FLV tag header **/
+        private function buildHeader(array : ByteArray) : void {
+            /* following specification http://download.macromedia.com/f4v/video_file_format_spec_v10_1.pdf */
             // Render header data
+            // ensure that we are at the beginning , for update case
+            array.position = 0;
             if (type == FLVTag.MP3_RAW) {
-                array = getTagHeader(TAG_TYPE_AUDIO, length + 1, pts);
+                updateTagHeader(array,TAG_TYPE_AUDIO, length + 1, pts);
                 // Presume MP3 is 44.1 stereo.
                 array.writeByte(0x2F);
             } else if (type == AVC_HEADER || type == AVC_NALU) {
-                array = getTagHeader(TAG_TYPE_VIDEO, length + 5, dts);
+                updateTagHeader(array,TAG_TYPE_VIDEO, length + 5, dts);
                 // keyframe/interframe switch (0x10 / 0x20) + AVC (0x07)
                 keyframe ? array.writeByte(0x17) : array.writeByte(0x27);
                 /* AVC Packet Type :
@@ -118,37 +156,16 @@ package org.mangui.hls.flv {
                 array.writeByte(compositionTime >> 8);
                 array.writeByte(compositionTime);
             } else if (type == DISCONTINUITY || type == METADATA) {
-                array = getTagHeader(FLVTag.TAG_TYPE_SCRIPT, length, pts);
+                updateTagHeader(array,FLVTag.TAG_TYPE_SCRIPT, length, pts);
             } else {
-                array = getTagHeader(TAG_TYPE_AUDIO, length + 2, pts);
+                updateTagHeader(array,TAG_TYPE_AUDIO, length + 2, pts);
                 // SoundFormat, -Rate, -Size, Type and Header/Raw switch.
                 array.writeByte(0xAF);
                 type == AAC_HEADER ? array.writeByte(0x00) : array.writeByte(0x01);
             }
-            for (var i : int = 0; i < pointers.length; i++) {
-                if (type == AVC_NALU) {
-                    array.writeUnsignedInt(pointers[i].length);
-                }
-                array.writeBytes(pointers[i].array, pointers[i].start, pointers[i].length);
-            }
-            // Write previousTagSize and return data.
-            array.writeUnsignedInt(array.length);
-            return array;
+            return;
         }
 
-        /** Returns the bytesize of the frame. **/
-        private function get length() : int {
-            var length : int = 0;
-            for (var i : int = 0; i < pointers.length; i++) {
-                length += pointers[i].length;
-                // Account for NAL startcode length.
-                if (type == AVC_NALU) {
-                    length += 4;
-                }
-            }
-            return length;
-        }
-        ;
         CONFIG::LOGGING {
             public function get typeString() : String {
                 switch(type) {
@@ -171,21 +188,26 @@ package org.mangui.hls.flv {
                 }
             }
         }
+
         /** push a data pointer into the frame. **/
         public function push(array : ByteArray, start : int, length : int) : void {
             pointers.push(new TagData(array, start, length));
+            length += length;
+            if (type == AVC_NALU) {
+                length += 4;
+            }
         }
-        ;
 
         /** Trace the contents of this tag. **/
         public function toString() : String {
             return "TAG (type: " + type + ", pts:" + pts + ", dts:" + dts + ", length:" + length + ")";
         }
-        ;
 
         public function clone() : FLVTag {
             var cloned : FLVTag = new FLVTag(this.type, this.pts, this.dts, this.keyframe);
+            cloned.builtData = this.builtData;
             cloned.pointers = this.pointers;
+            cloned.length = this.length;
             return cloned;
         }
     }

src/org/mangui/hls/demux/AACDemuxer.as
@@ -73,6 +73,7 @@ package org.mangui.hls.demux {
             }
             var adifTag : FLVTag = new FLVTag(FLVTag.AAC_HEADER, id3.timestamp, id3.timestamp, true);
             adifTag.push(adif, 0, adif.length);
+            adifTag.build();
             audioTags.push(adifTag);
 
             var audioTag : FLVTag;
@@ -87,6 +88,7 @@ package org.mangui.hls.demux {
                 } else {
                     audioTag.push(_data, frames[i].start, _data.length - frames[i].start);
                 }
+                audioTag.build();
                 audioTags.push(audioTag);
                 i++;
             }

src/org/mangui/hls/demux/MP3Demuxer.as
@@ -68,6 +68,7 @@
                 } else {
                     audioTag.push(_data, frames[i].start, _data.length - frames[i].start);
                 }
+                audioTag.build();
                 audioTags.push(audioTag);
                 i++;
             }

src/org/mangui/hls/demux/TSDemuxer.as
@@ -292,6 +292,7 @@ package org.mangui.hls.demux {
                         if (_curNalUnit && _curNalUnit.length) {
                             _curVideoTag.push(_curNalUnit, 0, _curNalUnit.length);
                         }
+                        _curVideoTag.build();
                         _tags.push(_curVideoTag);
                         _curVideoTag = null;
                         _curNalUnit = null;
@@ -376,6 +377,7 @@ package org.mangui.hls.demux {
                     Log.debug("TS/AAC: insert ADIF TAG");
                 }
                 adifTag.push(adif, 0, adif.length);
+                adifTag.build();
                 _tags.push(adifTag);
                 _adifTagInserted = true;
             }
@@ -388,6 +390,7 @@ package org.mangui.hls.demux {
                 stamp = Math.round(pes.pts + j * 1024 * 1000 / frame.rate);
                 var curAudioTag : FLVTag = new FLVTag(FLVTag.AAC_RAW, stamp, stamp, false);
                 curAudioTag.push(pes.data, frame.start, frame.length);
+                curAudioTag.build();
                 _tags.push(curAudioTag);
             }
             if (frame) {
@@ -421,6 +424,7 @@ package org.mangui.hls.demux {
             _audioPESfound=true;
             var tag : FLVTag = new FLVTag(FLVTag.MP3_RAW, pes.pts, pes.dts, false);
             tag.push(pes.data, pes.payload, pes.data.length - pes.payload);
+            tag.build();
             _tags.push(tag);
         };
 
@@ -473,6 +477,7 @@ package org.mangui.hls.demux {
                         }
                         // only push current tag if AVC HEADER has been pushed already
                         if(_avcc) {
+                            _curVideoTag.build();
                             _tags.push(_curVideoTag);
                         }
                         CONFIG::LOGGING {
@@ -592,7 +597,7 @@ package org.mangui.hls.demux {
                                             data.writeByte(0x11);
                                             data.writeObject(cc_data);
                                             tag.push(data, 0, data.length);
-
+                                            tag.build();
                                             _tags.push(tag);
                                         }
                                         else
@@ -615,6 +620,7 @@ package org.mangui.hls.demux {
                 _avcc = avcc;
                 var avccTag : FLVTag = new FLVTag(FLVTag.AVC_HEADER, pes.pts, pes.dts, true);
                 avccTag.push(avcc, 0, avcc.length);
+                avccTag.build();
                 // Log.debug("TS:AVC:push AVC HEADER");
                 _tags.push(avccTag);
             }
@@ -701,6 +707,7 @@ package org.mangui.hls.demux {
             // then write the ByteArray
             data.writeObject(pespayload);
             tag.push(data, 0, data.length);
+            tag.build();
             _tags.push(tag);
         }
 

src/org/mangui/hls/model/Fragment.as
@@ -83,6 +83,7 @@ package org.mangui.hls.model {
                 data.writeObject(id3_tag.data);
             }
             tag.push(data, 0, data.length);
+            tag.build();
             return tag;
         }
 
@@ -95,6 +96,7 @@ package org.mangui.hls.model {
             data.writeObject(this.seqnum);
             data.writeObject(this.duration);
             tag.push(data, 0, data.length);
+            tag.build();
             return tag;
         }
 

commit 51c44b70795f9367b2d29e6391a8d9302ac67ba6

FLVTag : fix regression introduced by https://github.com/mangui/flashls/commit/4147ac4b5a3c0036b557ba4fd3675a44c01bf2fe
fix ambiguity between length class member and length parameter

src/org/mangui/hls/flv/FLVTag.as
@@ -190,11 +190,13 @@ package org.mangui.hls.flv {
         }
 
         /** push a data pointer into the frame. **/
-        public function push(array : ByteArray, start : int, length : int) : void {
-            pointers.push(new TagData(array, start, length));
-            length += length;
-            if (type == AVC_NALU) {
-                length += 4;
+        public function push(array : ByteArray, start : int, len : int) : void {
+            if(len) {
+                pointers.push(new TagData(array, start, len));
+                length += len;
+                if (type == AVC_NALU) {
+                    length += 4;
+                }
             }
         }
 