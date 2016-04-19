7097f7054260124ffeffe41c9d7332b2c5eb98f7
Neil Rackett <neil.rackett@mesmotronic.com>
2016-04-19 09:22:34 +0100

    Updated comments in build-temp.bat

--- build/build-temp.bat
+++ build/build-temp.bat
@@ -1,5 +1,5 @@
 @echo off
 
-REM Using the -temp switch builds the SWF/SWC files into build-temp so they're ignore by git
+REM Builds the SWF/SWC files into build-temp so they're ignore by git
 
 build ..\bin-temp
